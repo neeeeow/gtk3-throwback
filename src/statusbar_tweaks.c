c22a0d1579be9f4d89e0a47ae9e8b4e9c3577c68
neeeeow <166566700+neeeeow@users.noreply.github.com>
2025-06-12 16:44:01 +0100

    Add copyright notice to files

--- src/statusbar_tweaks.c
+++ src/statusbar_tweaks.c
@@ -1,3 +1,24 @@
+/*
+
+	gtk3-throwback
+	GtkStatusBar tweaks
+	Copyright (C) 2025 neeeeow (https://github.com/neeeeow/gtk3-throwback)
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ */
+
 #include "statusbar_tweaks.h"
 
 static void (*orig_statusbar_realize)(GtkWidget*);
